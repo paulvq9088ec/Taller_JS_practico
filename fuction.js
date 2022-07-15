56587dfc2e3c9600c240c6d5600999aedad81642
Paul Veintimilla <paulvq9088@hotmail.com>
2022-07-14 21:42:30 -0500

    Este es el primer avance del curso de JavaScrip de Platzi

---
+++ fuction.js
@@ -0,0 +1,9 @@
+function promedio(){
+    location.href = "promedio/promedio.html";
+}
+function mediana(){
+    location.href = "mediana/mediana.html";
+}
+function moda(){
+    location.href = "moda/moda.html";
+}
