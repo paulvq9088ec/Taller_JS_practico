bac3e1f8b7ba635537dee5a0e1ab6604c13b1c04
Paul Veintimilla <paulvq9088@hotmail.com>
2022-07-20 22:32:32 -0500

    Segunda Actualización Promedio y mediana

--- promedio/promedio.js
+++ promedio/promedio.js
@@ -3,11 +3,21 @@ function home(){
 }
 
 
+function calcularMediaAritmetica (lista) {
+    // let sumaLista = 0;
 
-const lista1 = [100, 200, 300, 500,];
+    // for(let i = 0; i < lista.length; i++){
+    //     sumaLista = sumaLista + lista[i];
+    // }
+    
+    const sumaLista = lista.reduce(
+        function (valorAcumulado = 0, nuevoElemento) {
+            return valorAcumulado + nuevoElemento;
+        }
+    );
+    
+    const promedioLista = sumaLista / lista.length;
 
-let sumaLista1 = 0;
+    return promedioLista
 
-for(let i = 0; i < lista1.length; i++){
-    sumaLista1 = sumaLista1 + lista1[i];
 }
